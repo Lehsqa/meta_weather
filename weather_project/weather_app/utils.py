94683a9799fac12a49ca214b0348a0eef7a304e3
devRSV <devRSV@d.c>
2023-05-28 21:59:55 +0300

    Update Docker file and utils.py

--- weather_project/weather_app/utils.py
+++ weather_project/weather_app/utils.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from .models import Weather
 from datetime import datetime, timedelta
 
 
 def update_weather():
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=chrome_options)
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--headless=new')
+    options.add_argument('--start-maximized')
+    options.add_argument("--enable-javascript")
+    driver = webdriver.Chrome(options=options)
 
     url = "https://pogoda.meta.ua/ua/Kyivska/Kyivskiy/Kyiv/month/"
     driver.get(url)
